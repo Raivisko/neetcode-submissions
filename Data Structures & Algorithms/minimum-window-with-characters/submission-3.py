a2610f6c4b01fdfff43fd7f72502c01e5cbc16a3
Raivisko <raivis.kondrasovs@gmail.com>
2026-05-13 19:36:21 +0300

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t)>len(s): return ''
+        
+        dict1 = {}
+        dict2 = {}
+        
+        for n in range(len(t)):
+            if t[n] not in dict1:
+                dict1[t[n]] = 1
+               
+            else:
+                dict1[t[n]] += 1
+        need=len(dict1)        
+        print(dict1, need)
+
+        l = 0
+        have=0
+        length = float('inf')
+        resl=0
+        resr=0
+
+        for r in range(len(s)):
+            if s[r] not in dict2 and s[r] in t:
+                dict2[s[r]] = 1
+                if dict2[s[r]] == dict1.get(s[r], 0):
+                    have += 1
+            elif s[r] in dict2 and s[r] in t:
+                dict2[s[r]]+=1
+                if dict2[s[r]] == dict1.get(s[r], 0):
+                    have += 1
+            # if have==need:
+            while have==need:
+                if r - l + 1 < length:
+                    length = r - l + 1
+                    resl, resr = l, r
+                print(resl,resr, length)
+                if s[l] in dict2:
+                    dict2[s[l]] -=1
+                    if dict2[s[l]] < dict1.get(s[l], 0):
+                        have -= 1
+                l+=1
+                print(l, dict2)
+        if length<float('inf'):
+            return s[resl:resr+1]
+        return ''
+                
+        print(dict2, have)     
